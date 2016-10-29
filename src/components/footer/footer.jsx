6f0072afe1f224ff5bb3027fb1327d0d88e80c8a
Adil Wali <adil@adil.io>
2016-10-29 16:02:32 -0700

    Styling the footer.

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,26 +12,6 @@ const Footer = () : HTMLElement => {
   return (
     <div styleName="footer">
       <div styleName="wrap">
-        <div styleName="links">
-          <ul>
-            <h3>Company</h3>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Jobs</a></li>
-            <li><a href="#">Gift Cards</a></li>
-          </ul>
-          <ul>
-            <h3>Help</h3>
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">Shipping</a></li>
-            <li><a href="#">Returns</a></li>
-          </ul>
-          <ul>
-            <h3>Stores</h3>
-            <li><a href="#">New York, NY</a></li>
-            <li><a href="#">Los Angeles, LA</a></li>
-            <li><a href="#">Las Vegas, NV</a></li>
-          </ul>
-        </div>
 
         <div styleName="social">
           <div styleName="social-links">
@@ -40,16 +20,21 @@ const Footer = () : HTMLElement => {
             <Icon name="fc-twitter" styleName="social-icon" />
             <Icon name="fc-pinterest" styleName="social-icon" />
           </div>
-          <TextInputWithLabel placeholder="Stay in to know" label={<Icon name="fc-arrow" />}/>
         </div>
-      </div>
 
-      <div styleName="copyright">
-        <p>&copy; Some Brand. All Rights Reserved</p>
-        <ul>
-          <li><a href="">Terms of Use</a></li>
-          <li><a href="">Privacy Policy</a></li>
-        </ul>
+        <div styleName="links">
+          <ul>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Shipping & Returns</a></li>
+          </ul>
+        </div>
+
+
+
+        <div styleName="copyright">
+          <p>&copy; Top Drawer</p>
+        </div>
       </div>
     </div>
   );
