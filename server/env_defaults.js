1df900b424bf82a2dc9834ce3a68f34b91a46683
Adil Wali <adil@adil.io>
2016-10-31 13:04:37 -0700

    Default to HTTPS locally.

--- server/env_defaults.js
+++ server/env_defaults.js
@@ -1,4 +1,4 @@
 
-process.env.API_URL = process.env.API_URL || 'http://localhost';
+process.env.API_URL = process.env.API_URL || 'https://localhost';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.FIREBRAND_LANGUAGE = process.env.FIREBRAND_LANGUAGE || 'en';
