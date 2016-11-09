389ec7caee826163be2e0f0cb9703916ad5e8c86
Adil Wali <adil@adil.io>
2016-11-08 18:13:54 -0800

    Taking out node_env=production for now until we can get images working.

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,4 +1,4 @@
-NODE_ENV=production gulp build
+gulp build
 docker build -t td-storefront .
 docker tag td-storefront td-docker.foxcommerce.com:5000/td-storefront
 docker push td-docker.foxcommerce.com:5000/td-storefront
