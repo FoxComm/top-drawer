b1743b751060e7b211594e4bde7f779551cf0cdc
Josh Ferrerra <josh@foxcommerce.com>
2016-11-30 10:37:07 -0500

    keep old home page for after holidays

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 import Site from './components/layout/site';
 import StoreFront from './components/layout/storefront';
 import Home from './pages/home/home';
+import HolidayHome from './pages/home/holiday-home';
 import Products from './pages/catalog/products';
 import Locations from './pages/locations/locations';
 import OurStory from './pages/our-story/our-story';
@@ -29,7 +30,7 @@ const routes = (
   <Route path="/" component={Site}>
     <Route path="/checkout" component={Checkout} />
     <Route component={StoreFront}>
-      <IndexRoute component={Home} />
+      <IndexRoute component={HolidayHome} />
       <Route path="/profile" component={ProfilePage}>
         <IndexRoute component={Profile} />
         <Route component={ProfileUnit}>
