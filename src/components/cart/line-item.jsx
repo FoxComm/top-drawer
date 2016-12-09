c8484e667c5c52fdb2b59c25a38572358a4f73c9
DemetriousWilson <DemetriousWilson@gmail.com>
2016-12-08 18:27:06 -0800

    Add Select component from perfect-gourment

--- src/components/cart/line-item.jsx
+++ src/components/cart/line-item.jsx
@@ -14,7 +14,7 @@ import localized from 'lib/i18n';
 // components
 import Icon from 'ui/icon';
 import Currency from 'ui/currency';
-import Autocomplete from 'ui/autocomplete';
+import Select from 'ui/select/select';
 
 const QUANTITY_ITEMS = _.range(1, 1 + 10, 1).map(x => x.toString());
 
@@ -29,23 +29,8 @@ type Props = {
   updateLineItemQuantity: Function,
 };
 
-type State = {
-  smallOnly: boolean,
-}
-
 class LineItem extends Component {
   props: Props;
-  _mqSmallOnly: any;
-
-  state: State = {
-    smallOnly: false,
-  };
-
-  componentDidMount() {
-    this._mqSmallOnly = window.matchMedia(`(max-width: 47.9375em)`);
-    this._mqSmallOnly.addListener(this.mediaQueryChanged);
-    this.setState({smallOnly: this._mqSmallOnly.matches});
-  }
 
   @autobind
   changeQuantity(quantity) {
@@ -56,47 +41,12 @@ class LineItem extends Component {
   deleteItem() {
     this.props.deleteLineItem(this.props.sku);
   }
+  quantityItems() {
+    if (this.props.quantity > 8) {
+      return _.range(1, this.props.quantity + 3, 1).map(x => x.toString());
+    }
 
-  @autobind
-  selectQuantity(event) {
-    const selectedQuantity = _.toInteger(event.target.value);
-    this.props.updateLineItemQuantity(this.props.sku, selectedQuantity);
-  }
-
-  @autobind
-  mediaQueryChanged() {
-    this.setState({smallOnly: this._mqSmallOnly.matches});
-  }
-
-  get mobileInput() {
-    return (
-      <div styleName="line-item-select-container">
-        <select value={this.props.quantity} onChange={this.selectQuantity}>
-          { QUANTITY_ITEMS.map(qty => {
-              return <option key={qty} value={qty}>{qty}</option>;
-            })
-          }
-        </select>
-      </div>
-    );
-  }
-
-  get desktopInput() {
-    return (
-      <div styleName="quantity">
-        <Autocomplete
-          inputProps={{
-            type: 'number',
-            readOnly: 'readOnly',
-          }}
-          getItemValue={item => item}
-          items={QUANTITY_ITEMS}
-          onSelect={this.changeQuantity}
-          selectedItem={this.props.quantity}
-          sortItems={false}
-        />
-      </div>
-    );
+    return QUANTITY_ITEMS;
   }
 
   render() {
@@ -114,10 +64,18 @@ class LineItem extends Component {
               <Currency value={this.props.totalPrice}/>
             </div>
           </div>
-          { this.state.smallOnly
-            ? this.mobileInput
-            : this.desktopInput
-          }
+          <div styleName="quantity">
+            <Select
+              inputProps={{
+                type: 'number',
+              }}
+              getItemValue={item => item}
+              items={this.quantityItems()}
+              onSelect={this.changeQuantity}
+              selectedItem={this.props.quantity}
+              sortItems={false}
+            />
+          </div>
         </div>
         <div styleName="controls">
           <a styleName="delete-button" onClick={this.deleteItem}>
