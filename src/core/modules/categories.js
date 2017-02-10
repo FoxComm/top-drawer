657da41d978f581c56d46cccb26916c5b36dcaed
mbektimirov <mbektimirov@gmail.com>
2017-02-10 22:08:50 +0500

    Sitemap generator

--- src/core/modules/categories.js
+++ src/core/modules/categories.js
@@ -3,23 +3,38 @@
 import { createReducer } from 'redux-act';
 import { createAsyncActions } from '@foxcomm/wings';
 
+const categories = [
+  {
+    id: 3,
+    name: 'classic',
+    description: 'Classic year-round looks.',
+    display: false,
+  },
+  {
+    id: 4,
+    name: 'modern',
+    description: 'Modern looks for the modern man.',
+    display: false,
+  },
+  {
+    id: 5,
+    name: 'all',
+    description: 'Our entire collection of Top Drawer socks.',
+    display: true,
+    navName: 'Shop',
+    subItems: [
+      { name: 'classic', navName: 'Classic' },
+      { name: 'modern', navName: 'Modern' },
+    ],
+  },
+];
+
 function apiCall(): Promise {
-  const result = [
-    {id: 3, name: 'classic', description: 'Classic year-round looks.', display: false},
-    {id: 4, name: 'modern', description: 'Modern looks for the modern man.', display: false},
-    {
-      id: 5,
-      name: 'all',
-      description: 'Our entire collection of Top Drawer socks.',
-      display: true,
-      navName: 'Shop',
-      subItems: [
-        { name: 'classic', navName: 'Classic' },
-        { name: 'modern', navName: 'Modern' },
-      ],
-    },
-  ];
-  return Promise.resolve(result);
+  return Promise.resolve(categories);
+}
+
+function convertCategoryNameToUrlPart(categoryName: string) {
+  return encodeURIComponent(categoryName.replace(/\s/g, '-'));
 }
 
 const initialState = {
@@ -39,4 +54,6 @@ const reducer = createReducer({
 export {
   reducer as default,
   fetch,
+  categories,
+  convertCategoryNameToUrlPart,
 };
