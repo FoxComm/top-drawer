d67c8f63732d9c5dcf706c2529f3770f90f3a460
Pavel Kiselev <kpashka@gmail.com>
2017-04-07 02:16:48 +0700

    Buildkite groups

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,13 @@
 set -ue
 
 # Cleanup dependencies
+@echo "---Cleanup"
 cd ../../
 rm -rf api-js
 rm -rf wings
 
 # Link api-js dependency
+@echo "---Linking api-js"
 git clone git@github.com:FoxComm/api-js.git
 cd api-js
 npm link
@@ -15,6 +17,7 @@ cd ../top-drawer
 npm link @foxcomm/api-js
 
 # Link wings dependency
+@echo "---Linking wings"
 cd ../
 git clone git@github.com:FoxComm/wings.git
 cd wings
@@ -23,7 +26,10 @@ cd ../top-drawer
 npm link @foxcomm/wings
 
 # Build
+@echo "---Building"
 cd top-drawer
 make build
 
-echo ${BUILDKITE_BRANCH}-stage
+# Final
+@echo "---Finale"
+@echo ${BUILDKITE_BRANCH}-stage
